bd44dc167dcf8eb3e7e5bb9ee6e786b59c53ad4c
Charlotte <ckdrew670@hotmail.com>
2019-12-29 23:31:49 +0000

    Add reset and reshuffle button and set up display ternary. App now works correctly but there are issues with styling of list items

--- src/Settings.js
+++ src/Settings.js
@@ -15,6 +15,8 @@ class Settings extends Component {
         this.handleName = this.handleName.bind(this);
         this.handleAdd = this.handleAdd.bind(this);
         this.handleCreateTeams = this.handleCreateTeams.bind(this);
+        this.handleReshuffle = this.handleReshuffle.bind(this);
+        this.handleReset = this.handleReset.bind(this);
     }
 
 handleName(e) {
@@ -31,13 +33,11 @@ handleAdd(e) {
 
     name !== "" ?
         this.setState({ 
-            
             names: [...names, name],
             name: ""
         }) : 
 
         this.setState({ 
-            
             names: [...names],
             name: ""
         });
@@ -49,14 +49,6 @@ shuffleArray(array) {
         let n = Math.floor(Math.random() * (i + 1));
         [array[i], array[n]] = [array[n], array[i]];
     }
-}
-
-filterTeams(array) {
-    let teamA = array.slice(0,(array.length/2));
-    let teamB = array.slice(array.length/2);
-    console.log([teamA, teamB]);
-    
-    
 }
 
 handleCreateTeams(e) {
@@ -72,26 +64,56 @@ handleCreateTeams(e) {
     }) 
     
 }
+
+handleReshuffle(e) {
+    
+    let { names } = this.state;
+    
+    e.preventDefault();
+    //this.props.handleSave(name);
+
+    this.setState({ 
+        displayForm: false,
+        shuffledNames: this.shuffleArray(names),
+    }) 
+    
+}
+
+handleReset(e) {
+    
+    let { names, displayForm } = this.state;
+    
+    e.preventDefault();
+    //this.props.handleSave(name);
+
+    this.setState({ 
+        displayForm: true,
+        names: [],
+    }) 
+    
+}
+
 render() {
     
-    let { names, filterTeams } = this.state;
+    let { names, displayForm } = this.state;
     
     return (
-        <>
+        
+            displayForm ?
+            <>
             <section className="settings">
 
                 <h2>Enter Player Name:</h2>
-
                 <form 
                     className="form" 
                     onSubmit={ this.handleAdd }>
-                
+                    
                     <input
                         className="form-input"
                         type="text"
                         onChange={ this.handleName }
                         value={ this.state.name }
-                        placeholder="e.g. Harry Potter"
+                        placeholder=" e.g. Luke Skywalker"
                     />
                         
                     <button 
@@ -107,16 +129,60 @@ render() {
                             className="list-group-item"
                             key={ index }
                         >{ value }</li>
-                    )) : <p>Add a player in the box above.</p> }
+                    )) : <p className="instructions">Add each player by typing their name in the box above. You can add any number of players to the list. <br/> When you are done, click 'Create Teams' to split the players into two random teams.</p>
+                }
                 </ul>     
                 
                 <button 
                     className="button-create"
                     onClick={ this.handleCreateTeams }
-                    >Create Teams</button>
+                >Create Teams</button>
+
+            </section>
+            </>
+            :
+            
+            <section className="team-sheet">
+                <div className="team-card">
+                    <h2>Team A</h2>
+                    <ul className="list-group">
+                        { names.filter((value, i) => (
+                            i % 2 === 0 ?
+                            <li 
+                                className="list-group-item"
+                                key={ i }
+                            >{ value }</li>
+                        :
+                        null ))
+                        }
+                    </ul>    
+                </div>
+
+                {/* <div className="team-card">
+                    <h2>Team B</h2>
+                    <ul className="list-group">
+                        { names.filter((value, i) => (
+                            i % 2 !== 0 ?
+                            <li 
+                                className="list-group-item"
+                                key={ i }
+                            >{ value }</li>
+                        :
+                        null ))}
+                    </ul>    
+                </div> */}
+                <button 
+                    className="button-reshuffle"
+                    onClick={ this.handleReshuffle }
+                >Reshuffle Teams</button>
+
+                <button 
+                    className="button-create"
+                    onClick={ this.handleReset }
+                >Create New Teams</button>
+
             </section>
             
-        </>
     ); }
 }
 export default Settings;
